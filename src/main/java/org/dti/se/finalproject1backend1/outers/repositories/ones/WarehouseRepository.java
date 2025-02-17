acd52c2a91512ed889b4cd6c37a1f6a217ec33da
norman1208 <achmadnorman1208@gmail.com>
2025-02-18 04:17:46 +0800

    fix find warehouse id by account id

--- src/main/java/org/dti/se/finalproject1backend1/outers/repositories/ones/WarehouseRepository.java
+++ src/main/java/org/dti/se/finalproject1backend1/outers/repositories/ones/WarehouseRepository.java
@@ -2,6 +2,8 @@ package org.dti.se.finalproject1backend1.outers.repositories.ones;
 
 import org.dti.se.finalproject1backend1.inners.models.entities.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +11,6 @@ import java.util.UUID;
 
 @Repository
 public interface WarehouseRepository extends JpaRepository<Warehouse, UUID> {
-    List<UUID> findWarehouseIdsByAccountId(UUID accountId);
+    @Query("SELECT wa.warehouse.id FROM WarehouseAdmin wa WHERE wa.account.id = :accountId")
+    List<UUID> findWarehouseIdsByAccountId(@Param("accountId") UUID accountId);
 }
